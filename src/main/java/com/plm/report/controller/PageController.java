09ffeb793dc5a34237d838e85dd0d2545cad66b1
dcy <dcy@family.com>
2026-05-08 09:30:20 +0800

    update

---
+++ src/main/java/com/plm/report/controller/PageController.java
@@ -0,0 +1,24 @@
+package com.plm.report.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class PageController {
+
+    @GetMapping({"/", "/report-list"})
+    public String reportListPage() {
+        return "forward:/report-list.html";
+    }
+
+    @GetMapping("/report-view")
+    public String reportViewPage() {
+        return "forward:/report-view.html";
+    }
+
+    @GetMapping("/report-view/{id}")
+    public String reportViewById(@PathVariable("id") String id) {
+        return "redirect:/report-view.html?id=" + id;
+    }
+}
