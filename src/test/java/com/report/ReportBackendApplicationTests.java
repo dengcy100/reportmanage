2065815b3ddbb664322c6deba44622fc633afbb3
dcy <dcy@family.com>
2026-05-10 15:52:34 +0800

    update

--- src/test/java/com/report/ReportBackendApplicationTests.java
+++ src/test/java/com/report/ReportBackendApplicationTests.java
@@ -1,19 +1,10 @@
 package com.report;
 
-import com.report.config.DatabaseMigrationRunner;
-import com.report.config.SeederRunner;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class ReportManageApplicationTests {
-
-	@MockBean
-	private DatabaseMigrationRunner databaseMigrationRunner;
-
-	@MockBean
-	private SeederRunner seederRunner;
 
 	@Test
 	void contextLoads() {
